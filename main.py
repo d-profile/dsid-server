89bf78c3a975f87b4a2a3288a077d115fc010a68
Bui Duc Huy <duchuy.124dk@gmail.com>
2023-03-22 15:40:22 +0700

    setup

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+from server.services.student import StudentService
+import uvicorn
 
 app = FastAPI()
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World 11"}
+async def root() -> dict:
+    await StudentService.register()
+    return {
+        "message": "hello"
+    }
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
---
+++ server/services/student.py
@@ -0,0 +1,6 @@
+class StudentService(object):
+    @staticmethod
+    async def register() -> dict:
+        return {
+            "message": "Ok"
+        }
